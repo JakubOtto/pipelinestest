24c3fec8a47ecb57deeef8dd70a274a10b221c53
Jakub Otto <227225@student.uek.krakow.pl>
2024-10-02 17:27:02 +0200

    feat: test

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,7 +4,6 @@ import { Controller, Get } from '@nestjs/common';
 export class AppController {
   @Get()
   getHello(): string {
-    console.log('GET / route called'); // Dodaj log
     return 'Hello i::team!';
   }
 }
